c0ad73cca9b117917c0d8cd626703ddb572da73f
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-27 00:57:52 +0300

    create a method that defines the attributes of each instance of a user object

--- user.py
+++ user.py
@@ -6,3 +6,9 @@ create a User class from which we will generate and save new user accounts
 
 class User:
     users_list = [] #empty list, which will hold all users
+    """
+    Method that defines the attributes of each user account.
+    """
+    def __init__(self, user_name, password ):
+        self.user_name = user_name
+        self.password = password
