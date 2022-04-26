df3d1480d3718f57454d4f36e32cb140a56d8c2e
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-27 01:05:55 +0300

    add the test class that defines test cases for the behaviour in the User class

--- user_test.py
+++ user_test.py
@@ -1,3 +1,9 @@
 import unittest
 from user import User
 
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for the User class behaviour.
+    args: 
+    unittest.Testcase class that helps in creating test cases.
+    """
