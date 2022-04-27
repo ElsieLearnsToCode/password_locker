b7dc46143e1dd1ae43eeaf5b4db59d87195d72ca
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-27 06:46:43 +0300

    create a method that defines the attributes of user credentials

--- credentials.py
+++ credentials.py
@@ -2,3 +2,15 @@
 class Credentials:
     
     existing_credentials = []
+
+    """
+    Method that defines the attributes of each credential that a user creates
+    """
+
+    def __init__(self, website, user_name, password):
+        
+        self.website = website
+        self.user_name = user_name
+        self.password = password
+
+        
