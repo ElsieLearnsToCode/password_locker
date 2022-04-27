29c3883238f6cd7bdbb3692ec964d66031600d0d
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-04-27 06:59:24 +0300

    create the test class that defines test cases for the Credentials class

---
+++ credentials_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+    """
+    Test class that defines test cases for the Credentials class behaviour.
+    args: 
+    unittest.Testcase class that helps in creating test cases.
+    """
